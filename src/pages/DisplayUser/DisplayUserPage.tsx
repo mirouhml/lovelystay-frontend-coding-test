842760e24a1b5abb93d73bbd69ed606ffdf00c5f
Ammar Hamlaoui <amarhamlaoui@gmail.com>
2023-02-06 15:39:40 +0100

    Implement DisplayUserPage

--- src/pages/DisplayUser/DisplayUserPage.tsx
+++ src/pages/DisplayUser/DisplayUserPage.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import UserInfo from '../../components/UserInfo/UserInfo';
+import RepositoriesList from '../../components/RepositoriesList/RepositoriesList';
 
 const DisplayUserPage = () => {
   return (
     <div>
       <UserInfo />
+      <RepositoriesList />
     </div>
   );
 };
