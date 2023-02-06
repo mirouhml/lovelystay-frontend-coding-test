a8610e64ce0e069fd2ac0fe3e94ab4629112c5d5
Ammar Hamlaoui <amarhamlaoui@gmail.com>
2023-02-06 15:38:36 +0100

    Update API to retrieve 10 repositories per page

--- src/services/githubAPI.ts
+++ src/services/githubAPI.ts
@@ -1,4 +1,4 @@
-import { User, Repo } from '../types';
+import { User, Repository } from '../types';
 
 export const fetchUser = async (username: string): Promise<User> => {
   try {
@@ -11,6 +11,7 @@ export const fetchUser = async (username: string): Promise<User> => {
       avatar_url: data.avatar_url,
       url: data.url,
       public_repos: data.public_repos,
+      number_of_repo_pages: Math.ceil(data.public_repos / 10),
     };
     return user;
   } catch (error) {
@@ -20,14 +21,14 @@ export const fetchUser = async (username: string): Promise<User> => {
 
 export const fetchRepos = async (
   username: string,
-  page: number
-): Promise<Repo[]> => {
+  page: string
+): Promise<Repository[]> => {
   try {
     const response = await fetch(
-      `https://api.github.com/users/${username}/repos?page=${page}`
+      `https://api.github.com/users/${username}/repos?page=${page}&per_page=10`
     );
     const data = await response.json();
-    const repos: Repo[] = data.map((repo: any) => {
+    const repos: Repository[] = data.map((repo: any) => {
       return {
         id: repo.id,
         name: repo.name,
