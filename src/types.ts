1b34de6367b3d760011853552110748d159ac6ae
Ammar Hamlaoui <amarhamlaoui@gmail.com>
2023-02-06 15:36:46 +0100

    Update types

--- src/types.ts
+++ src/types.ts
@@ -5,9 +5,10 @@ export type User = {
   avatar_url: string;
   url: string;
   public_repos: number;
+  number_of_repo_pages: number;
 };
 
-export type Repo = {
+export type Repository = {
   id: string;
   name: string;
   description: string;
