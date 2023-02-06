2c2ec56fff26bc9315c6ad2a15f452c0962e44c3
Ammar Hamlaoui <amarhamlaoui@gmail.com>
2023-02-06 04:43:51 +0100

    Add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import DisplayUserPage from './pages/DisplayUser/DisplayUserPage';
+import SearchUserPage from './pages/SearchUser/SearchUser';
 
 function App() {
-  return <div className='App'>Github Users</div>;
+  return (
+    <div className='App'>
+      <BrowserRouter>
+        <main>
+          <Routes>
+            <Route path='/' element={<SearchUserPage />} />
+            <Route path='/:username' element={<DisplayUserPage />} />
+          </Routes>
+        </main>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
