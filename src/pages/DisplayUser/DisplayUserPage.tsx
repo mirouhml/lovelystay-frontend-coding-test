c69da78f610abace796436a6d15a47cfdff19919
Ammar Hamlaoui <amarhamlaoui@gmail.com>
2023-02-06 04:44:42 +0100

    Create DisplayUserPage

---
+++ src/pages/DisplayUser/DisplayUserPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import UserInfo from '../../components/UserInfo/UserInfo';
+
+const DisplayUserPage = () => {
+  return (
+    <div>
+      <UserInfo />
+    </div>
+  );
+};
+
+export default DisplayUserPage;
