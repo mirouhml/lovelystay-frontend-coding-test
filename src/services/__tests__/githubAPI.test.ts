cc7241e7c314ce162633cd904d135f43a50a9d7c
Ammar Hamlaoui <amarhamlaoui@gmail.com>
2023-02-07 18:10:46 +0100

    Adjust test

--- src/services/__tests__/githubAPI.test.ts
+++ src/services/__tests__/githubAPI.test.ts
@@ -1,23 +1,7 @@
 import { fetchUser, fetchRepos } from '../githubAPI';
-import fetch from 'jest-fetch-mock';
 
 describe('fetchUser', () => {
-  beforeEach(() => {
-    fetch.resetMocks();
-  });
-
   it('should return a user object', async () => {
-    fetch.mockResponseOnce(
-      JSON.stringify({
-        id: 1,
-        login: 'johndoe',
-        name: 'John Doe',
-        avatar_url: 'https://avatars.githubusercontent.com/u/1?v=4',
-        url: 'https://api.github.com/users/johndoe',
-        public_repos: 2,
-      }),
-      { status: 200 }
-    );
     const user = await fetchUser('johndoe');
     expect(user).toEqual({
       id: 1,
@@ -31,13 +15,6 @@ describe('fetchUser', () => {
   });
 
   it('should return an error message if the user is not found', async () => {
-    fetch.mockResponseOnce(
-      JSON.stringify({
-        message: 'Not Found',
-      }),
-      { status: 404 }
-    );
-
     const user = await fetchUser('not-a-user');
     expect(user).toEqual({
       message: 'User not found',
@@ -45,7 +22,6 @@ describe('fetchUser', () => {
   });
 
   it('should return an error message if something goes wrong', async () => {
-    fetch.mockRejectOnce(() => Promise.reject('API is down'));
     const user = await fetchUser('error');
     expect(user).toEqual({
       message: 'Something went wrong',
@@ -55,21 +31,6 @@ describe('fetchUser', () => {
 
 describe('fetchRepos', () => {
   it('should return an array of repositories', async () => {
-    fetch.mockResponseOnce(
-      JSON.stringify([
-        {
-          id: 1,
-          name: 'repo1',
-          description: 'This is repo1',
-        },
-        {
-          id: 2,
-          name: 'repo2',
-          description: 'This is repo2',
-        },
-      ]),
-      { status: 200 }
-    );
     const repos = await fetchRepos('johndoe', '1');
     expect(repos).toEqual([
       {
@@ -86,7 +47,6 @@ describe('fetchRepos', () => {
   });
 
   it('should return an error message if something goes wrong', async () => {
-    fetch.mockRejectOnce(() => Promise.reject('API is down'));
     const repos = await fetchRepos('error', '1');
     expect(repos).toEqual({
       message: 'Something went wrong',
