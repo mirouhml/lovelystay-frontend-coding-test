acf25a78117b33021719f8314e779d7d2f7c9b31
Ammar Hamlaoui <amarhamlaoui@gmail.com>
2023-02-08 20:18:33 +0100

    Fix router for gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import DisplayUserPage from './pages/DisplayUserPage';
 import SearchUserPage from './pages/SearchUserPage';
 
 function App() {
   return (
     <main>
-      <BrowserRouter>
+      <Router>
         <Routes>
           <Route path='/' element={<SearchUserPage />} />
           <Route path='/:username' element={<DisplayUserPage />} />
         </Routes>
-      </BrowserRouter>
+      </Router>
     </main>
   );
 }
