d6736b90dce0702ea3cbd03c87da6cc6fe1fbbf6
Ammar Hamlaoui <amarhamlaoui@gmail.com>
2023-02-09 01:32:02 +0100

    Add not found page

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HashRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './app/store';
 import './sass/main.scss';
@@ -11,7 +12,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </Provider>
   </React.StrictMode>
 );
